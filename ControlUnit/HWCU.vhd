9edc0dafd0c783e8a5fb38b93a7ef633de627a18
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2022-05-12 17:59:11 +0200

    feat : Started implementing lut

--- ControlUnit/HWCU.vhd
+++ ControlUnit/HWCU.vhd
@@ -1,6 +1,9 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.op_func.all;
+use work.cw_flags.all;
+
 entity HWCU is
     port (
         OPCODE   : in std_logic_vector(5 downto 0);
@@ -21,6 +24,17 @@ entity HWCU is
 end HWCU;
 
 architecture beh of HWCU is
+    subtype control_word is std_logic_vector(CW_IF_LEN+CW_ID_LEN+CW_EX_LEN+CW_WB_LEN-1 downto 0);
+    -- Define LUT type
+    type lut_t is array (0 to NO_INSTRUCTIONS-1) of control_word;
+    -- Declare the LUT of the hardwired CU
+    signal lookup_table : lut_t := (
+        --RTYPE Mapping
+        (SEL_PCSRC_0 OR EN_IF_ID_1) & --IF
+        (SEL_REGDST_1 OR EN_REGWRITE_1) & --ID
+        (BRANCH_0  OR JUMP_0 OR SEL_ALUSRC_0) & --EX
+        (SEL_MEMTOREG_1 OR SEL_JAL_0), --WB
+    );
 
 begin
 
