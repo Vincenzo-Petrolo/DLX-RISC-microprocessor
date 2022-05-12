60d50c959f855ccbb1481fac2d8e8344ce8fe1ce
Diamante Simone Crescenzo <s295783@studenti.polito.it>
2022-05-12 15:53:25 +0200

    Feat: added EX/MEM and MEM/WB pipeline registers and connections. Fix: fixed wrong interconnections among components and pipeline registers. FiX: Reg File Write address now follows correct path through pipeline registers.

--- Datapath/DP.vhd
+++ Datapath/DP.vhd
@@ -103,12 +103,22 @@ architecture struct of DP is
     signal PC_ADDER_REG_IF_ID_i: std_logic_vector(31 downto 0);
     signal INSTRMEM_REG_IF_ID_i: std_logic_vector(31 downto 0);
     signal PC_ADDER_REG_ID_EX_i: std_logic_vector(31 downto 0);
+    signal JLABEL_REG_ID_EX_i: std_logic_vector(31 downto 0);
+    --signal PC_OFFSET_ID_EX_i: std_logic_vector(31 downto 0);
     signal RF_DATA_OUT1_REG_ID_EX_i: std_logic_vector(31 downto 0);
     signal RF_DATA_OUT2_REG_ID_EX_i: std_logic_vector(31 downto 0);
     signal SE_IMM_REG_ID_EX_i: std_logic_vector(31 downto 0);
+    signal ADDR_WR_MUX_OUT_REG_ID_EX_i: std_logic_vector(4 downto 0);
+    signal PC_ADDER_REG_EX_MEM_i: std_logic_vector(31 downto 0);
     signal PC_BRANCH_ADDER_OUT_REG_EX_MEM_i: std_logic_vector(31 downto 0);
     signal ALU_OUT_REG_EX_MEM_i: std_logic_vector(31 downto 0);
     signal RF_DATA_OUT2_REG_EX_MEM_i: std_logic_vector(31 downto 0);
+    signal ADDR_WR_MUX_OUT_REG_EX_MEM_i: std_logic_vector(4 downto 0);
+    signal PC_ADDER_REG_MEM_WB_i: std_logic_vector(31 downto 0);
+    signal DATA_IN_REG_MEM_WB_i: std_logic_vector(31 downto 0);
+    signal ALU_OUT_REG_MEM_WB_i: std_logic_vector(31 downto 0);
+    signal ADDR_WR_MUX_OUT_REG_MEM_WB_i: std_logic_vector(4 downto 0);
+    
     
 begin
     
@@ -117,36 +127,44 @@ begin
     ADDR_R3_i <= INSTRMEM_REG_IF_ID_i(15 downto 11);
     SE_IMM_i(15 downto 0) <= INSTRMEM_REG_IF_ID_i(15 downto 0);
     SE_IMM_i(31 downto 16) <= (others => INSTRMEM_REG_IF_ID_i(15));
-    SHSE_IMM_i <= SE_IMM_i(29 downto 0)&"00";
-    JLABEL_i <= "0000" & INSTRMEM_REG_IF_ID_i(25 downto 0) & "00";  -- to be modified for successive pipeline stage
+    SHSE_IMM_i <= SE_IMM_REG_ID_EX_i(29 downto 0)&"00";
+    JLABEL_i <= "0000" & INSTRMEM_REG_IF_ID_i(25 downto 0) & "00";  -- (to be modified for successive pipeline stage)
     PC_OUT <= PC_OUT_i;
-    ALU_OUT <= ALU_OUT_i;
-    DATA_OUT <= DATA_OUT2_i;
+    ALU_OUT <= ALU_OUT_REG_EX_MEM_i;
+    DATA_OUT <= RF_DATA_OUT2_REG_EX_MEM_i;
     PcMuxSel <= Jump or (Branch and ZERO_i);
     
     PC: REG generic map (32) port map (CLK, RST, PC_IN_i, PC_OUT_i);
     PC_ADDER: ADDER port map (PC_OUT_i, x"00000004", PC_ADDER_OUT_i);
     ADDR_WR_MUX: MUX21 generic map (5) port map (ADDR_R2_i, ADDR_R3_i, RegDst, ADDR_WR_MUX_OUT_i); 
-    JAL_MUX : MUX21 generic map (32) port map (MEM_MUX_OUT_i, PC_ADDER_OUT_i, Jal, JAL_MUX_OUT_i);
-    REGFILE: RF port map (RST, RegWrite, ADDR_R1_i, ADDR_R2_i, ADDR_WR_MUX_OUT_i, JAL_MUX_OUT_i, DATA_OUT1_i, DATA_OUT2_i);
-    ALU_MUX: MUX21 port map (DATA_OUT2_i, SE_IMM_i, ALUSrc, ALU_MUX_OUT_i);
-    ALU_DP: ALU port map (DATA_OUT1_i, ALU_MUX_OUT_i, ALUOpcode, ALU_OUT_i, ZERO_i);
-    JUMP_MUX: MUX21 generic map (32) port map (JLABEL_i ,SHSE_IMM_i, PcMuxSel, PC_OFFSET_i);
+    JAL_MUX : MUX21 generic map (32) port map (MEM_MUX_OUT_i, PC_ADDER_REG_MEM_WB_i, Jal, JAL_MUX_OUT_i);
+    REGFILE: RF port map (RST, RegWrite, ADDR_R1_i, ADDR_R2_i, ADDR_WR_MUX_OUT_REG_MEM_WB_i, JAL_MUX_OUT_i, DATA_OUT1_i, DATA_OUT2_i);
+    ALU_MUX: MUX21 port map (RF_DATA_OUT2_REG_ID_EX_i, SE_IMM_REG_ID_EX_i, ALUSrc, ALU_MUX_OUT_i);
+    ALU_DP: ALU port map (RF_DATA_OUT1_REG_ID_EX_i, ALU_MUX_OUT_i, ALUOpcode, ALU_OUT_i, ZERO_i);
+    JUMP_MUX: MUX21 generic map (32) port map (JLABEL_REG_ID_EX_i ,SHSE_IMM_i, PcMuxSel, PC_OFFSET_i);
     PC_BRANCH_ADDER: ADDER port map (PC_ADDER_REG_ID_EX_i, PC_OFFSET_i, PC_BRANCH_ADDER_OUT_i);
-    PC_MUX: MUX21 port map (PC_ADDER_OUT_i, PC_BRANCH_ADDER_OUT_i, PCSrc, PC_IN_i);
-    MEM_MUX: MUX21 port map (DATA_IN, ALU_OUT_i, MemToReg, MEM_MUX_OUT_i);
+    PC_MUX: MUX21 port map (PC_ADDER_OUT_i, PC_BRANCH_ADDER_OUT_REG_EX_MEM_i, PCSrc, PC_IN_i);
+    MEM_MUX: MUX21 port map (DATA_IN_REG_MEM_WB_i, ALU_OUT_REG_MEM_WB_i, MemToReg, MEM_MUX_OUT_i);
 
     -- Pipeline REGS
     PC_ADDER_REG_IF_ID: REG generic map (32) port map (CLK, RST, PC_ADDER_OUT_i, PC_ADDER_REG_IF_ID_i);
     INSTRMEM_REG_IF_ID: REG generic map (32) port map (CLK, RST, INSTRUCTION, INSTRMEM_REG_IF_ID_i);
     PC_ADDER_REG_ID_EX: REG generic map (32) port map (CLK, RST, PC_ADDER_REG_IF_ID_i, PC_ADDER_REG_ID_EX_i);
+    JLABEL_REG_ID_EX: REG generic map (32) port map (CLK, RST, JLABEL_i, JLABEL_REG_ID_EX_i);
+    --PC_OFFSET_ID_EX: REG generic map (32) port map (CLK, RST, PC_OFFSET_i, PC_OFFSET_ID_EX_i);
     RF_DATA_OUT1_REG_ID_EX: REG generic map (32) port map (CLK, RST, DATA_OUT1_i, RF_DATA_OUT1_REG_ID_EX_i);
     RF_DATA_OUT2_REG_ID_EX: REG generic map (32) port map (CLK, RST, DATA_OUT2_i, RF_DATA_OUT2_REG_ID_EX_i);
-    SE_IMM_REG_ID_EX_i: REG generic map (32) port map (CLK, RST, SE_IMM_i, SE_IMM_REG_ID_EX_i);
+    SE_IMM_REG_ID_EX: REG generic map (32) port map (CLK, RST, SE_IMM_i, SE_IMM_REG_ID_EX_i);
+    ADDR_WR_MUX_OUT_REG_ID_EX: REG generic map (5) port map (CLK, RST, ADDR_WR_MUX_OUT_i, ADDR_WR_MUX_OUT_REG_ID_EX_i);
+    PC_ADDER_REG_EX_MEM: REG generic map (32) port map (CLK, RST, PC_ADDER_REG_ID_EX_i, PC_ADDER_REG_EX_MEM_i);
     PC_BRANCH_ADDER_OUT_REG_EX_MEM: REG generic map (32) port map (CLK, RST, PC_BRANCH_ADDER_OUT_i, PC_BRANCH_ADDER_OUT_REG_EX_MEM_i);
     ALU_OUT_REG_EX_MEM: REG generic map (32) port map (CLK, RST, ALU_OUT_i, ALU_OUT_REG_EX_MEM_i);
     RF_DATA_OUT2_REG_EX_MEM: REG generic map (32) port map (CLK, RST, RF_DATA_OUT2_REG_ID_EX_i, RF_DATA_OUT2_REG_EX_MEM_i);
-    -- to restart from adding JLABEL_i register ID/EX
+    ADDR_WR_MUX_OUT_REG_EX_MEM: REG generic map (5) port map (CLK, RST, ADDR_WR_MUX_OUT_REG_ID_EX_i, ADDR_WR_MUX_OUT_REG_EX_MEM_i);
+    PC_ADDER_REG_MEM_WB: REG generic map (32) port map (CLK, RST, PC_ADDER_REG_EX_MEM_i, PC_ADDER_REG_MEM_WB_i);
+    DATA_IN_REG_MEM_WB: REG generic map (32) port map (CLK, RST, DATA_IN, DATA_IN_REG_MEM_WB_i);
+    ALU_OUT_REG_MEM_WB: REG generic map (32) port map (CLK, RST, ALU_OUT_REG_EX_MEM_i, ALU_OUT_REG_MEM_WB_i);
+    ADDR_WR_MUX_OUT_REG_MEM_WB: REG generic map (5) port map (CLK, RST, ADDR_WR_MUX_OUT_REG_EX_MEM_i, ADDR_WR_MUX_OUT_REG_MEM_WB_i);
 
     
 end struct;
