81bf985f4d1b99bb910ba16bd5c1f83789a485e7
Vincenzo Petrolo <vincenzo.petrolo99@gmail.com>
2022-06-08 21:42:31 +0200

    chore : simulation works, added READMEs for reproducing both sim and syn

--- sim/assembly_programs/division_custom.asm
+++ sim/assembly_programs/division_custom.asm
@@ -1,6 +1,21 @@
-addi r1, r0, 81
-addi r2, r0, 9
-xor r3, r3, r3
+	addi r1, r0, #81
+	addi r2, r0, #9
+	jal proc_iterative_division
+	nop
+	nop
+after_division_proc:
+	j stop
+	nop
+	nop
+
+
+; =====Procedure Iterative Division=====
+;Gets on r1,r2 dividend and divisor 
+;and store the result both in memory at address 0 and 
+;and on register r5
+
+proc_iterative_division:
+	xor r3, r3, r3
 	nop
 	nop
 	nop
@@ -8,9 +23,7 @@ divide:
 	sne r5, r1, r2
 	nop
 	nop
-	nop
 	beqz r5, finish
-	nop
 	nop
 	nop
 	sub r1, r1, r2
@@ -18,16 +31,21 @@ divide:
 	j divide
 	nop
 	nop
-	nop
 finish:
-add r4, r0, r1
-nop
-nop
-nop
-sw 0(r0), r4
-lw r5, 0(r0)
+	add r4, r0, r1
+	nop
+	nop
+	nop
+	sw 0(r0), r4
+	lw r5, 0(r0)
+	j after_division_proc
+	nop
+	nop
+; =====End Procedure Iterative Division=====
+
+
 stop: 
-j stop
-nop
-nop
-nop
+	j stop
+	nop
+	nop
+	nop
